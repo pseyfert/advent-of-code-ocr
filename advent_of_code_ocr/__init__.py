a0eb45ed4e91fb29bbe461d12c22b3604af68d3f
Paul Seyfert <pseyfert.mathphys@gmail.com>
2021-12-13 21:33:05 +0100

    black

--- advent_of_code_ocr/__init__.py
+++ advent_of_code_ocr/__init__.py
@@ -7,9 +7,16 @@ from .characters import ALPHABET_6
 __version__ = "0.2.0"
 
 
-def convert_6(input_text: str, *, fill_pixel: str = "#", empty_pixel: str = "."):
-    array = np.array([[char == fill_pixel for char in row]
-                     for row in input_text.split("\n")], dtype=bool)
+def convert_6(
+    input_text: str, *, fill_pixel: str = "#", empty_pixel: str = "."
+):
+    array = np.array(
+        [
+            [char == fill_pixel for char in row]
+            for row in input_text.split("\n")
+        ],
+        dtype=bool,
+    )
 
     return convert_6_np(array)
 
@@ -17,7 +24,7 @@ def convert_6(input_text: str, *, fill_pixel: str = "#", empty_pixel: str = ".")
 def convert_6_np(array):
     rows, cols = array.shape
     if (trunk := cols % 5) < 4:
-        array = array[:, :-(1+trunk)]
+        array = array[:, : -(1 + trunk)]
     rows, cols = array.shape
 
     if rows != 6:
@@ -25,8 +32,8 @@ def convert_6_np(array):
 
     result = ""
 
-    for character in range(1 + cols//5):
-        tmp = array[:, 5*character:5*(character+1)-1]
+    for character in range(1 + cols // 5):
+        tmp = array[:, 5 * character : 5 * (character + 1) - 1]
         string = "\n".join("".join("#" if e else "." for e in r) for r in tmp)
 
         letter = ALPHABET_6[string]
