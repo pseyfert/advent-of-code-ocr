d16d9274bc818da3024694c6ebf188023f8f0516
Paul Seyfert <pseyfert.mathphys@gmail.com>
2021-12-13 14:46:29 +0100

    cleanup

--- advent_of_code_ocr/__init__.py
+++ advent_of_code_ocr/__init__.py
@@ -27,9 +27,7 @@ def convert_6_np(array):
 
     for character in range(1 + cols//5):
         tmp = array[:, 5*character:5*(character+1)-1]
-        if tmp.shape !=(6,4):
-            continue
-        string = "\n".join( "".join("#" if e else "." for e in r) for r in tmp)
+        string = "\n".join("".join("#" if e else "." for e in r) for r in tmp)
 
         letter = ALPHABET_6[string]
         result += letter
